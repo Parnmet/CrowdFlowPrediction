8a8a980e5e85bd92073d5c1565820652a6af76a8
Pittayarat <pittayarat.sae@gmail.com>
2017-01-20 00:58:01 +0700

    can predict in sameDay and lastDay

--- DensePrediction.py
+++ DensePrediction.py
@@ -1,29 +1,41 @@
 
-d1 = [0,1,2,3,4,5,6,7,8,9]
-d2 = [1,1,2,3,4,5,6,7,8,8]
-d3 = [2,1,2,3,4,5,6,7,8,10]
+d1 = [0,1,2,3,4,5,6,7,8,7]
+d2 = [1,2,4,4,4,5,6,7,8,8]
+d3 = [2,3,2,3,4,5,6,7,8,9]
+newd = [1,2]
  
-table = [d3,d3,d3,d2,d2,d2,d1]
+table = [d1,d1,d1,d3,d3,d3,d2,newd]
 
-next_time = 0
-current_time = 9
-current_value = table[-1][9]
-table_len = len(table)-1
+allPeriod = 10
+last_length = len(table[-1])
+if last_length == allPeriod:
+    current_time = allPeriod-1
+    next_time = 0    
+else:
+    current_time = last_length-1
+    next_time = current_time+1  
+table_len = len(table)-1  
+current_value = table[-1][current_time]
 
 equal_dense = []
 #[{index,next_value},{...},...]
  
 #find equal dense
-for index in range(table_len):
+for index in range(table_len): 
     if table[index][current_time] == current_value:
-        equal_dense.append({"index":index,"next_value":table[index+1][next_time]})
-
-while len(equal_dense) == 0:
-    count = 1
-    for index in range(table_len):          
-        if table[index][current_time] == current_value-count or table[index][current_time] == current_value+count:
-            print
+        if last_length == allPeriod:
             equal_dense.append({"index":index,"next_value":table[index+1][next_time]})
+        else:
+            equal_dense.append({"index":index,"next_value":table[index][next_time]})
+count = 1
+          
+while len(equal_dense) == 0:
+    for index in range(table_len): 
+        if (table[index][current_time] == current_value-count) or (table[index][current_time] == current_value+count):            
+            if last_length == allPeriod:
+                equal_dense.append({"index":index,"next_value":table[index+1][next_time]})
+            else:
+                equal_dense.append({"index":index,"next_value":table[index][next_time]})     
     count+=1
 
 if len(equal_dense) == 1:
@@ -49,9 +61,6 @@ elif len(equal_dense) > 1:
         filterList = list(filter(lambda d: d['freq'] in [max_freq], freq)) 
         max_index = max(filterList, key=lambda k: k['latest_index'])
         predict=max_index['value']
-
-# print(equal_dense)          
-    
 
 print(predict)
 
