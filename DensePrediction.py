54b0d0eb3315a4016d165862bc943894aab3200f
Pittayarat <pittayarat.sae@gmail.com>
2017-01-27 03:06:07 +0700

    predict dense from txt file

--- DensePrediction.py
+++ DensePrediction.py
@@ -1,67 +1,59 @@
 
-d1 = [0,1,2,3,4,5,6,7,8,7]
-d2 = [1,2,4,4,4,5,6,7,8,8]
-d3 = [2,3,2,3,4,5,6,7,8,9]
-newd = [1,2]
- 
-table = [d1,d1,d1,d3,d3,d3,d2,newd]
 
-allPeriod = 10
-last_length = len(table[-1])
-if last_length == allPeriod:
-    current_time = allPeriod-1
-    next_time = 0    
-else:
-    current_time = last_length-1
-    next_time = current_time+1  
-table_len = len(table)-1  
-current_value = table[-1][current_time]
+def findNextDense(table):
+    allPeriod = 288
+    last_length = len(table[-1])
+    if last_length == allPeriod:
+        current_time = allPeriod-1
+        next_time = 0    
+    else:
+        current_time = last_length-1
+        next_time = current_time+1  
+    table_len = len(table)-1  
+    current_value = table[-1][current_time]
 
-equal_dense = []
-#[{index,next_value},{...},...]
- 
-#find equal dense
-for index in range(table_len): 
-    if table[index][current_time] == current_value:
-        if last_length == allPeriod:
-            equal_dense.append({"index":index,"next_value":table[index+1][next_time]})
-        else:
-            equal_dense.append({"index":index,"next_value":table[index][next_time]})
-count = 1
-          
-while len(equal_dense) == 0:
+    equal_dense = []
+    #[{index,next_value},{...},...]
+    
+    #find equal dense
     for index in range(table_len): 
-        if (table[index][current_time] == current_value-count) or (table[index][current_time] == current_value+count):            
+        if table[index][current_time] == current_value:
             if last_length == allPeriod:
                 equal_dense.append({"index":index,"next_value":table[index+1][next_time]})
             else:
-                equal_dense.append({"index":index,"next_value":table[index][next_time]})     
-    count+=1
+                equal_dense.append({"index":index,"next_value":table[index][next_time]})
+    count = 1
+            
+    while len(equal_dense) == 0:
+        for index in range(table_len): 
+            if (table[index][current_time] == current_value-count) or (table[index][current_time] == current_value+count):            
+                if last_length == allPeriod:
+                    equal_dense.append({"index":index,"next_value":table[index+1][next_time]})
+                else:
+                    equal_dense.append({"index":index,"next_value":table[index][next_time]})     
+        count+=1
 
-if len(equal_dense) == 1:
-    predict = equal_dense[0]['next_value']
-elif len(equal_dense) > 1:
-    freq = [] 
-    #[{value,latest_index,freq},{...},...]
-    for e in equal_dense:
-        found = False
-        for f in freq:
-            if e['next_value'] == f['value']:
-                if e['index'] > f['latest_index']:
-                    f['latest_index'] = e['index']
-                f['freq'] +=1
-                found = True
-                break
-        if len(freq) == 0 or found == False:
-            freq.append({"value":e['next_value'],"latest_index":e['index'],"freq":1})
-    if len(freq) == 1:
-        predict = freq[0]['value']
-    elif len(freq)>1:
-        max_freq = max(freq, key=lambda k: k['freq'])['freq']
-        filterList = list(filter(lambda d: d['freq'] in [max_freq], freq)) 
-        max_index = max(filterList, key=lambda k: k['latest_index'])
-        predict=max_index['value']
-
-print(predict)
-
-
+    if len(equal_dense) == 1:
+        predict = equal_dense[0]['next_value']
+    elif len(equal_dense) > 1:
+        freq = [] 
+        #[{value,latest_index,freq},{...},...]
+        for e in equal_dense:
+            found = False
+            for f in freq:
+                if e['next_value'] == f['value']:
+                    if e['index'] > f['latest_index']:
+                        f['latest_index'] = e['index']
+                    f['freq'] +=1
+                    found = True
+                    break
+            if len(freq) == 0 or found == False:
+                freq.append({"value":e['next_value'],"latest_index":e['index'],"freq":1})
+        if len(freq) == 1:
+            predict = freq[0]['value']
+        elif len(freq)>1:
+            max_freq = max(freq, key=lambda k: k['freq'])['freq']
+            filterList = list(filter(lambda d: d['freq'] in [max_freq], freq)) 
+            max_index = max(filterList, key=lambda k: k['latest_index'])
+            predict=max_index['value']
+    return predict
---
+++ DenseTable.py
@@ -0,0 +1,78 @@
+import DensePrediction
+import json
+import datetime
+import csv
+
+# d1 = [0,1,2,3,4,5,6,7,8,7]
+# d2 = [1,2,4,4,4,5,6,7,8,8]
+# d3 = [2,3,2,3,4,5,6,7,8,9]
+# newd = [1,2]
+ 
+# table = [d1,d1,d1,d3,d3,d3,d2,newd]
+# table
+
+allPeriod = 288
+Start = "2017-01-01"
+dayStart = datetime.datetime.strptime(Start, "%Y-%m-%d")
+End = "2017-01-20"
+dayEnd = datetime.datetime.strptime(End, "%Y-%m-%d")
+
+dayCheckStart = dayStart-datetime.timedelta(days=30)
+
+table = []
+prediction = {}
+prediction['predict'] = []
+csvPredict = []
+csvDate = []
+
+with open('SanamluangTweet_None.json') as json_data:
+    tweetJSON = json.load(json_data)
+    tweetList = tweetJSON['tweet']
+
+    #init 30 day table for predict
+    for i in range(30):
+        dayCheckString = dayCheckStart.strftime("%Y-%m-%d")
+        dayGet = [_  for _ in tweetList if _["date"] == dayCheckString]
+        if dayGet != []:
+            dayList = dayGet[0]['dense']
+        else:
+            dayList = [0]*allPeriod
+        table.append(dayList)
+        dayCheckStart+= datetime.timedelta(days=1)
+    
+    dayPredict = dayStart
+    
+    #allDayPediction
+    while dayPredict <= dayEnd:
+        dayPredictString = dayPredict.strftime("%Y-%m-%d")        
+        realdense = [_  for _ in tweetList if _["date"] == dayPredictString]
+        if realdense != []:
+            dayList = realdense[0]['dense']
+        else:
+            dayList = [0]*allPeriod 
+        oneDayPredict = []
+        #1 day prediction
+        for i in range(allPeriod):
+            predict = DensePrediction.findNextDense(table)
+            oneDayPredict.append(predict)
+            #addRealDense
+            if len(table[-1]) == allPeriod:
+                table.append([dayList[i]])
+            else:
+                table[-1].append(dayList[i])
+        csvPredict.append(oneDayPredict)
+        csvDate.append(dayPredictString)
+        prediction['predict'].append({"date":dayPredictString,"dense":oneDayPredict})
+        dayPredict += datetime.timedelta(days=1)        
+
+    with open('SanamluangTweet_Predict.json', 'w') as outfile:
+        json.dump(prediction, outfile)
+    
+    csvRow = [list(i) for i in zip(*csvPredict)]
+    with open('SanamluangTweet_Predict.csv', 'w', newline='') as fp:
+        a = csv.writer(fp, delimiter=',')
+        a.writerows([csvDate])
+        a.writerows(csvRow)
+    # print(csvRow)
+
+
